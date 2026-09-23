6a877fc990954a83695b5e62c657a32329413139
homutovan <homutovan@live.com>
2026-09-23 10:27:09 +0700

    fix: update example hw_2, task 3

--- example/hw_2_t3.py
+++ example/hw_2_t3.py
@@ -2,13 +2,35 @@
 
 number = 100
 
-while True:
-    request_number = int(number / 2)
-    user_answer = input(f"Ваше число больше {request_number}? (да/нет):")
+# задаем начальный диапазон чисел от 0 до N
+
+th_1 = 0                                            # нижняя граница диапазона поиска     
+th_2 = th_1 + number                                # верхняя граница диапазона поиска 
+count = 0                                           # счетчик попыток
+
+while th_1 < th_2:
+    count += 1
+    mean = (th_1 + th_2) // 2                       # середина диапазона поиска 
+    print(f"Пробуем угадать число, попытка {count}")
+    print(f"Диапазона: {th_1}, {th_2}, середина: {mean}")
+
+    user_answer = input(f"Ваше число больше {mean}? (да/нет):")
+    user_answer = user_answer.strip().lower() 
 
     if user_answer == "да":
-        number = number + request_number
+        print(f"Загаданное число больше {mean}")
+        th_1 = mean + 1                             # число точно больше середины диапазона
+
     elif user_answer == "нет":
-        number = request_number
-    elif user_answer == "это оно":
-        print(f"Я угадал ваше число! Это {request_number}!")
+        print(f"Загаданное число меньше {mean}")
+        th_2 = mean                                 # число ≤ середины диапазона
+
+    else:
+        print("Пожалуйста, ответьте да или нет")
+        count -= 1
+        continue
+
+    print("\n")
+
+print(f"Ваше число: {th_1}")
+print(f"Количество вопросов: {count}")
